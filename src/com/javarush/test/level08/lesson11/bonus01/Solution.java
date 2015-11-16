b239156d46a705f4eac91fb70947996a28ac8b8b
Benefic <abenefic@gmail.com>
2015-11-16 19:59:27 +0300

    up

---
+++ src/com/javarush/test/level08/lesson11/bonus01/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level08.lesson11.bonus01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* Номер месяца
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: «May is 5 month».
+Используйте коллекции.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sMonth = reader.readLine();
+        Map<String, String> months = new HashMap<String, String>();
+        months.put("January", "1");
+        months.put("February", "2");
+        months.put("March", "3");
+        months.put("April", "4");
+        months.put("May", "5");
+        months.put("June", "6");
+        months.put("July", "7");
+        months.put("August", "8");
+        months.put("September", "9");
+        months.put("October", "10");
+        months.put("November", "11");
+        months.put("December", "12");
+        System.out.println(sMonth + " is " + months.get(sMonth) + " month");
+    }
+
+}
