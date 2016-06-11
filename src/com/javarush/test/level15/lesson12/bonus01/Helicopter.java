e6d74271441351a87ae352b68b0f07a951a64569
Benefic <abenefic@gmail.com>
2016-06-11 23:16:49 +0300

    l15

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Helicopter.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by Benefuc on 11.06.2016.
+ */
+public class Helicopter implements Flyable {
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by Benefuc on 11.06.2016.
+ */
+public class Plane implements Flyable
+{
+    @Override
+    public void fly() {
+
+    }
+
+    public Plane (int passCount){
+
+    }
+}
